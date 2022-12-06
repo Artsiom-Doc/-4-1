f478a830ca7e1b3427a328e43292cc2546d05d49
Artsiom <Frt@mail.ru>
2022-12-06 19:25:15 +0300

    Найдено большее bp 3х чисел

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if(b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine(max);
